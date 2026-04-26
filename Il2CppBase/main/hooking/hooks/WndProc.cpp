848ee0e8df2d010fef1a83b9cb34134231db102f
ViniLog789 <devka.damax@bk.ru>
2026-04-26 12:51:49 +0500

    Add support Unity 6000 (Fix menu ignored clicks)

--- Il2CppBase/main/hooking/hooks/WndProc.cpp
+++ Il2CppBase/main/hooking/hooks/WndProc.cpp
@@ -2,36 +2,38 @@
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-LRESULT __stdcall Hooks::HkWndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT __stdcall Hooks::HkWndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) // add support Unity 6000
 {
+    /*
     POINT mPos;
     GetCursorPos(&mPos);
     ScreenToClient(Renderer::Window, &mPos);
 
     ImGui::GetIO().MousePos.x = mPos.x;
     ImGui::GetIO().MousePos.y = mPos.y;
-
-    if (uMsg == WM_KEYUP && wParam == MenuToggleKey)
-    {
-        menu.Toggle();
-    }
-
-    if (uMsg == WM_KEYUP && wParam == VK_END)
-    {
-        NOTIFY_WARNING_QUICK("You have pressed the emergency unload button. Goodbye.");
-        Globals::CheatWork = false;
-    }
-
-    if (uMsg == WM_KEYUP && wParam == VK_HOME)
-    {
-        Globals::MenuToggleKey = VK_RETURN;
-        NOTIFY_INFO_QUICK("The menu opening button has been reset.");
-    }
+    */
 
     if (menu.Open)
     {
-        ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam);
-        return true;
+        if (ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
+            return true;
+    }
+
+    if (uMsg == WM_KEYUP)
+    {
+        if (wParam == MenuToggleKey)
+            menu.Toggle();
+
+        else if (wParam == VK_END)
+        {
+            NOTIFY_WARNING_QUICK("Emergency unload...");
+            Globals::CheatWork = false;
+        }
+        else if (wParam == VK_HOME)
+        {
+            Globals::MenuToggleKey = VK_RETURN;
+            NOTIFY_INFO_QUICK("Menu key reset to ENTER.");
+        }
     }
 
     return CallWindowProc(hooking->OriginalWndproc, hWnd, uMsg, wParam, lParam);
--- Il2CppBase/main/menu/menu.cpp
+++ Il2CppBase/main/menu/menu.cpp
@@ -364,7 +364,7 @@ void Menu::Render()
 
                 ImGui::Dummy(ImVec2(0, 8));
                 ImGui::TextColored(ImVec4(0.6f, 0.8f, 1.0f, 1.0f), "Special Thanks");
-                ImGui::TextWrapped("Evelien (feature system)");
+                ImGui::TextWrapped("Evelien (feature system), .gashopeless");
             }
             ImGui::EndChild();
 
